592b11d923dc6665d8ca48646a2016a2a98fbcf0
Eduardo Augusto <161646703+EduardoARO@users.noreply.github.com>
2024-06-25 01:22:23 -0300

    FIX perfil.js
    Pedidos agora serão exibidos de acordo com o usuário logado

--- routes/perfil.js
+++ routes/perfil.js
@@ -4,9 +4,15 @@ const router = express.Router();
 // Rota GET para renderizar a página do perfil
 router.get('/perfil', async (req, res) => {
     try {
-        const users = await req.db.collection('user_tb').find().toArray();
-        const pedidos = await req.db.collection('pedido_tb').find().toArray();
-        res.render('perfil', { users, pedidos, user: req.session.user });
+        const user = req.session.user; // Supondo que o usuário logado está na sessão
+        if (!user) {
+            return res.status(401).send('Usuário não está autenticado.');
+        }
+
+        // Buscando apenas os pedidos do usuário logado
+        const pedidos = await req.db.collection('pedido_tb').find({ email: user.email }).toArray();
+        
+        res.render('perfil', { user, pedidos });
     } catch (error) {
         console.error(error);
         res.status(500).send('Erro ao buscar dados.');
